f5893318ecdd1568c75f08b1ff6e6a13e2a87f7a
kanoflor <kanoflordelis@gmail.com>
2025-07-14 17:03:59 +0200

    Configure default options for QueryClient in react-query.ts

--- lib/react-query.ts
+++ lib/react-query.ts
@@ -1,3 +1,13 @@
 import { QueryClient } from '@tanstack/react-query';
 
-export const queryClient = new QueryClient();
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 3,
+      staleTime: 5 * 60 * 1000, // 5 minutes
+    },
+    mutations: {
+      retry: 1,
+    },
+  },
+});
